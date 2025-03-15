b44eafdbd647cc9c6b5658245f935e3dbd671dec
Danussh P <danusshp@csod.com>
2025-03-16 03:07:38 +0530

    commit poc layout

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,22 +1,193 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import "./Dashboard.css";
+import { FaBars, FaTimes } from "react-icons/fa";
+import avatarImage from "../../assets/avatar.png";
 
+import { FaFileUpload } from "react-icons/fa";
+import { FaTable } from "react-icons/fa";
+import { FaChartPie } from "react-icons/fa";
+import { FaUpload } from "react-icons/fa";
+import success from "../../assets/success.png";
+import PleaseScreen from "../Pages/PleaseScreen.js";
 const Dashboard = () => {
-  const navigate = useNavigate();
+  const [activePage, setActivePage] = useState("home");
+  const [stockFile, setStockFile] = useState(null);
+  const [salesFile, setSalesFile] = useState(null);
+  const [uploadComplete, setUploadComplete] = useState(false);
 
-  const handleLogout = () => {
-    navigate("/");
+  const handleNavigation = (page) => {
+    setActivePage(page);
   };
 
+  const handleFileChange = (event, type) => {
+    if (type === "stock") {
+      setStockFile(event.target.files[0]);
+    } else if (type === "sales") {
+      setSalesFile(event.target.files[0]);
+    }
+  };
+
+  const removeFile = (type) => {
+    if (type === "stock") {
+      setStockFile(null);
+    } else if (type === "sales") {
+      setSalesFile(null);
+    }
+  };
+
+  const handleStart = () => {
+    if (uploadComplete) {
+      setActivePage("analytics");
+    }
+  };
+
+  useEffect(() => {
+    if (stockFile && salesFile) {
+      setUploadComplete(true);
+      console.log("stockFile", stockFile);
+      console.log("salesFile", salesFile);
+    } else {
+      setUploadComplete(false);
+    }
+  }, [stockFile, salesFile]);
+
+  console.log("uploadComplete", uploadComplete);
+
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Welcome to the Dashboard</h1>
-      <button
-        onClick={handleLogout}
-        style={{ padding: "10px 20px", marginTop: "20px", cursor: "pointer" }}
-      >
-        Logout
-      </button>
+    <div className="dashboard-wrapper">
+      <div className="dashboard-container">
+        {/* Sidebar */}
+        <aside className="sidebar">
+          <button className="menu-button">
+            <FaBars />
+          </button>
+          <nav className="nav-icons">
+            <FaChartPie
+              className={`icon ${activePage === "home" ? "active" : ""}`}
+              onClick={() => handleNavigation("home")}
+            />
+            <FaFileUpload
+              className={`icon ${activePage === "profile" ? "active" : ""}`}
+              onClick={() => handleNavigation("profile")}
+            />
+            <FaTable
+              className={`icon ${activePage === "settings" ? "active" : ""}`}
+              onClick={() => handleNavigation("settings")}
+            />
+          </nav>
+          <div className="profile-pic">
+            <img src={avatarImage} alt="Profile" />
+          </div>
+        </aside>
+
+        {/* Main Content */}
+        <main className="main-content">
+          {activePage === "home" && (
+            <>
+              <div className="start-section">
+                <button
+                  className="start-button"
+                  disabled={!uploadComplete}
+                  style={{ background: uploadComplete ? "#26B297" : "grey" }}
+                  onClick={handleStart}
+                >
+                  Start
+                </button>
+              </div>
+              <div className="upload-section">
+                <div className="upload-wrapper-1">
+                  {stockFile ? (
+                    <div className="upload-box-1">
+                      <img
+                        src={success}
+                        alt="success"
+                        className="edc-success"
+                      />
+                    </div>
+                  ) : (
+                    <div className="upload-box-1">
+                      <input
+                        type="file"
+                        id="file1"
+                        onChange={(e) => handleFileChange(e, "stock")}
+                      />
+                      <span className="upload-icon">
+                        <FaUpload />
+                      </span>
+
+                      <p className="upload-text">
+                        Upload <strong>Stock Data</strong> File
+                      </p>
+                      <small>Drag and drop here</small>
+                    </div>
+                  )}
+
+                  {stockFile && (
+                    <div className="file-chip">
+                      <span>{stockFile.name}</span>
+                      <FaTimes
+                        className="remove-icon"
+                        onClick={() => removeFile("stock")}
+                      />
+                    </div>
+                  )}
+                </div>
+                <div className="upload-wrapper">
+                  {salesFile ? (
+                    <div className="upload-box-2">
+                      <img
+                        src={success}
+                        alt="success"
+                        className="edc-success"
+                      />
+                    </div>
+                  ) : (
+                    <div className="upload-box-2">
+                      <input
+                        type="file"
+                        id="file2"
+                        onChange={(e) => handleFileChange(e, "sales")}
+                      />
+                      <span className="upload-icon">
+                        <FaUpload />
+                      </span>
+                      <p className="upload-text">
+                        Upload <strong>Sales Data</strong> File
+                      </p>
+                      <small>Drag and drop here</small>
+                    </div>
+                  )}
+                  {salesFile && (
+                    <div className="file-chip-2">
+                      <span>{salesFile.name}</span>
+                      <FaTimes
+                        className="remove-icon"
+                        onClick={() => removeFile("sales")}
+                      />
+                    </div>
+                  )}
+                </div>
+              </div>
+            </>
+          )}
+          {activePage === "analytics" && <PleaseScreen />}
+
+          {activePage === "home" && (
+            <div className="info-section">
+              <h2 className="info-title">FORECAST AI</h2>
+              <p className="info-description">
+                Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                accusantium doloremque laudantium.
+              </p>
+            </div>
+          )}
+
+          {/* Bhaskar Content */}
+
+          {activePage === "profile" && <h2>Profile Page</h2>}
+          {activePage === "settings" && <h2>Settings Page</h2>}
+        </main>
+      </div>
     </div>
   );
 };
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,44 +1,167 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./Login.css";
 import loginImage from "../../assets/login.png"; // Adjust path as needed
+import sqsImage from "../../assets/images.jpeg"; // Adjust path as needed
+
+import "./Login.css";
 
 const Login = () => {
-  const navigate = useNavigate();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
   const handleLogin = () => {
-    // Simple authentication check
-    if (username === "admin" && password === "password") {
+    if (username && password) {
       navigate("/dashboard");
-    } else {
-      alert("Invalid credentials! Try admin/password");
     }
   };
 
   return (
     <div className="login-container">
       <img src={loginImage} alt="Background" className="background-image" />
+      <div style={styles.loginContainer}>
+        <div style={styles.loginBox}>
+          <div style={styles.topBanner}></div>
 
-      <div className="login-box">
-        <h2>Login</h2>
-        <input
-          type="text"
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button onClick={handleLogin}>Login</button>
+          <img src={sqsImage} alt="Background" className="logo" />
+
+          <h2 style={styles.heading}>LOGIN</h2>
+          <input
+            type="text"
+            placeholder="User Name"
+            style={styles.inputField}
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            style={styles.inputField}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <div style={styles.buttonContainer}>
+            <button
+              style={{
+                ...styles.loginButton,
+                background: username && password ? "#26B297" : "#a5d6a7",
+                cursor: username && password ? "pointer" : "not-allowed",
+              }}
+              onClick={handleLogin}
+              disabled={!username || !password}
+            >
+              Login
+            </button>{" "}
+            <a href="#" style={styles.forgotPassword}>
+              Forgot Password?
+            </a>
+          </div>
+          <div style={styles.signupContainer}>
+            <a href="#" style={styles.link}>
+              Signup for a New Account
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
+};
+
+const styles = {
+  loginContainer: {
+    margin: 0,
+    padding: 0,
+    width: "100%",
+    height: "100vh",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    paddingLeft: "10%",
+  },
+  topBanner: {
+    position: "relative",
+    top: -70,
+    left: -60,
+    width: "80%",
+    height: 55,
+    background: "linear-gradient(to right, #12CA95, #8FEDAD)",
+    borderTopLeftRadius: "80px",
+    borderTopRightRadius: "0px",
+    borderBottomRightRadius: "140px",
+    // transform: "skewX(-20deg)",
+    // position: 'relative',
+    // left: -60,
+    // top: -70,,
+    // width: 80%,
+    // height: 55px,
+    // background: linear-gradient(to right, rgb(18, 202, 149), rgb(143, 237, 173)),
+    // border-top-left-radius: 80px,
+    // border-top-right-radius: 0px,
+    // border-bottom-right-radius: 140px,
+  },
+  loginBox: {
+    background: "white",
+    padding: "60px",
+    borderRadius: "50px",
+    boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
+    width: "450px",
+    textAlign: "center",
+  },
+  logoPlaceholder: {
+    width: "100px",
+    height: "50px",
+    background: "#ddd",
+    margin: "0 auto 20px",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    fontSize: "14px",
+    fontWeight: "bold",
+    color: "#666",
+  },
+  heading: {
+    marginBottom: "20px",
+    color: "#333",
+    textAlign: "left",
+  },
+  inputField: {
+    width: "100%",
+    padding: "10px",
+    margin: "8px 0",
+    border: "1px solid #ccc",
+    borderRadius: "5px",
+    fontSize: "16px",
+  },
+  buttonContainer: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginTop: "10px",
+  },
+  loginButton: {
+    padding: "10px 20px",
+    background: "#26B297",
+    color: "white",
+    border: "none",
+    borderRadius: "5px",
+    fontSize: "16px",
+    cursor: "pointer",
+    width: "35%",
+  },
+  forgotPassword: {
+    textDecoration: "none",
+    color: "#666",
+    fontSize: "14px",
+  },
+  signupContainer: {
+    marginTop: "15px",
+    textAlign: "center",
+  },
+  link: {
+    textDecoration: "none",
+    color: "#666",
+    fontSize: "14px",
+  },
 };
 
 export default Login;
