771d18604485b805f9329ea3a31f2124d3404906
Kotu Bhaskara Rao <bhaskara@squareshift.co>
2025-03-16 14:28:47 +0000

    Remove title

--- src/components/ExternalSiteInfo/Iframe.jsx
+++ src/components/ExternalSiteInfo/Iframe.jsx
@@ -3,29 +3,14 @@ import { Box, Paper } from "@mui/material";
 
 const Iframe = ({ src }) => {
   return (
-    <Box
-      sx={{
-        justifyContent: "center",
-        alignItems: "center",
+    <iframe
+      src={src}
+      style={{
+        width: "100%",
+        height: "100vh",
       }}
-    >
-      <Paper
-        elevation={0}
-        sx={{
-          height: "100vh",
-          overflow: "hidden", // Prevents parent scroll
-        }}
-      >
-        <iframe
-          src={src}
-          style={{
-            width: "100%",
-            height: "100%",
-          }}
-          title="Dynamic Iframe"
-        ></iframe>
-      </Paper>
-    </Box>
+      title="Dynamic Iframe"
+    ></iframe>
   );
 };
 
--- src/components/ExternalSiteInfo/LookerIframe.jsx
+++ src/components/ExternalSiteInfo/LookerIframe.jsx
@@ -13,26 +13,15 @@ const LookerIframe = () => {
         height: "100vh", // Ensures it doesn't overflow viewport
       }}
     >
-      {/* Properly Centered Title */}
-      <Typography 
-        variant="h4" 
-        fontWeight="bold" 
-        sx={{ textAlign: "center", paddingBottom: 1 }} // Ensures proper centering
-      >
-        Dashboard
-      </Typography>
 
       {/* Iframe Wrapper to Control Size */}
       <Box
         sx={{
           width: "100%", // Keeps iframe responsive
-          height: "85vh", // Prevents overflow
-          overflow: "hidden", // Prevents iframe from exceeding boundaries
-          borderRadius: 2,
-          boxShadow: 3, // Adds slight elevation
+          overflow: "hidden", // Prevents iframe from exceeding boundaries      
         }}
       >
-        {/* Iframe Component */}        
+        {/* Iframe Component */}
         <Iframe src="https://squareshift.cloud.looker.com/embed/dashboards/331?Distributor+Name=Audio+House&Sales+Date=7+day" />
       </Box>
     </Box>
